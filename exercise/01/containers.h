5e485c22f78fbb89c8a63cf7ba13d6f8582a9e05
Nenad Caklovic <nenad.caklovic@avl.com>
2018-11-14 23:55:26 +0100

    add exercise 01

---
+++ exercise/01/containers.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <list>
+
+void remove_element(std::vector<int>& v, int index);
+void input_element(std::vector<std::wstring>& v, int index, const std::wstring& value);
+
+int list_nth_element(const std::list<int>& c, int index);
+void list_sort_desc(std::list<int>& c);
+
+int unique_numbers(std::wistream&);
+
+class word_frequency
+{
+public:
+	word_frequency(std::wistream&);
+	int frequency(const std::wstring& s);
+	int count();
+};
