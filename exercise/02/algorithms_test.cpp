3b7d2b897343095470be464774059f37bc3bf10c
Nenad Caklovic <nenad.caklovic@avl.com>
2018-11-15 08:37:10 +0100

    update comments in exercise 02

--- exercise/02/algorithms_test.cpp
+++ exercise/02/algorithms_test.cpp
@@ -15,7 +15,7 @@ public:
 	{
 		std::wstringstream ss(L"14 -78 22");
 		std::vector<int> v;
-		// read values from input stream into vector
+		// TODO: read values from input stream into vector
 
 		Assert::AreEqual(3u, v.size());
 		Assert::AreEqual(14, v[0]);
@@ -26,7 +26,7 @@ public:
 	TEST_METHOD(test_02a)
 	{
 		std::vector<int> v(10);
-		// fill vector with incremental values
+		// TODO: fill vector with incremental values
 		Assert::AreEqual(10u, v.size());
 		Assert::IsTrue(std::is_sorted(v.cbegin(), v.cend()));
 		Assert::AreEqual( 1, v[0]);
@@ -36,7 +36,7 @@ public:
 	{
 		// generate
 		std::vector<int> v(10);
-		// fill vector with incremental values (by 2)
+		// TODO: fill vector with incremental values (by 2)
 		Assert::IsTrue(std::is_sorted(v.cbegin(), v.cend()));
 		Assert::IsTrue(v.cend() == std::adjacent_find(v.cbegin(), v.cend(), [](int a, int b) { return b - a != 2;  }));
 		Assert::AreEqual(1, v[0]);
@@ -46,7 +46,7 @@ public:
 	TEST_METHOD(test_03a)
 	{
 		std::vector<int> v = { 1, 5, 10 } ;
-		// change all values in a vector
+		// TODO: change all values in a vector
 		Assert::AreEqual(3u, v.size());
 		Assert::AreEqual(1, v[0]);
 		Assert::AreEqual(125, v[1]);
@@ -54,12 +54,11 @@ public:
 	}
 	TEST_METHOD(test_03b)
 	{
-		// transform 2 containers
 		int x[] = { 3, 5, 10 };
 		std::vector<int> y = { 4, 12, 10 };
 		std::vector<double> d;
 
-		// calculate distances from origin (from x and y collections) to new vector
+		// TODO: calculate distances from origin (from x and y collections) to new vector
 		Assert::AreEqual(3u, d.size());
 		Assert::AreEqual(5., d[0]);
 		Assert::AreEqual(13., d[1]);
@@ -68,53 +67,53 @@ public:
 	TEST_METHOD(test_04a)
 	{
 		std::wstringstream ss(L"1.5 2.5 3.5");
-		auto res = // sum of all values in input stream
+		auto res = // TODO: sum of all values in input stream
 		Assert::AreEqual(7.5, res);
 	}
 	TEST_METHOD(test_04b)
 	{
 		std::vector<std::wstring> v { L"A", L"V", L"L", L"!" };
-		auto res = // concatenated string with additional prefix 
+		auto res = // TODO: concatenated string with additional prefix 
 		Assert::AreEqual(L"GO AVL!", res.c_str());
 	}
 	TEST_METHOD(test_04c)
 	{
 		struct person { std::wstring name; int age; };
 		std::vector<person> v{ {L"Pero", 33}, {L"Iva", 25} };
-		auto total_age = // sum of all ages
+		auto total_age = // TODO: sum of all ages
 		Assert::AreEqual(58, total_age);
 	}
 
 	TEST_METHOD(test_05a)
 	{
 		std::vector<int> v { -5, 8, 11, 0, -9, 77, -4 };
-		auto number_of_negative = // 
+		auto number_of_negative = // TODO: 
 		Assert::AreEqual(3, number_of_negative);
 	}
 	TEST_METHOD(test_05b)
 	{
 		std::vector<double> v { 1.5, 8, -11.23, 0, 1e10, 1e10, 1e10, 0, 99 };
-		auto number_of_invalid = // 
+		auto number_of_invalid = // TODO: 
 		Assert::AreEqual(3, number_of_invalid);
 	}
 	TEST_METHOD(test_05c)
 	{
 		struct point { int x, y; };
 		std::vector<point> v{ {1,1}, {-5,3}, {2,2}, {-7,-6}, {9,-4} };
-		auto number_in_first_quadrant = //
+		auto number_in_first_quadrant = // TODO: 
 		Assert::AreEqual(2, number_in_first_quadrant);
 	}
 
 	TEST_METHOD(test_06)
 	{
 		std::vector<int> v { 33, 16, 24, 41, 25, 19, 9 };
-		auto first_prime = // 
+		auto first_prime = // TODO: 
 		Assert::AreEqual(41, first_prime);
 	}
 	TEST_METHOD(test_07a)
 	{
 		std::vector<double> v{ 1e10, 8, -11.23, 0, 1e10, 1e10, 1e10, 0, 99 };
-		// change every invalid value with -1 
+		// TODO: change every invalid value (1e10) with -1 
 		Assert::AreEqual(-1., v[0]);
 		Assert::AreEqual(-1., v[4]);
 		Assert::AreEqual(-1., v[6]);
@@ -122,13 +121,13 @@ public:
 	TEST_METHOD(test_07b)
 	{
 		std::wstring s(L"ponedjeljak");
-		// change every vowel with x 
+		// TODO: change every vowel with x 
 		Assert::AreEqual(L"pxnxdjxljxk", s.c_str());
 	}
 	TEST_METHOD(test_08a)
 	{
 		std::vector<double> v{ 1e10, 8, -11.23, 0, 1e10, 1e10, 1e10, 0, 99 };
-		// delete all invalid values
+		// TODO: delete all invalid values (1e10)
 		Assert::AreEqual(5u, v.size());
 		Assert::AreEqual(8., v[0]);
 		Assert::AreEqual(99., v[4]);
@@ -137,14 +136,14 @@ public:
 	TEST_METHOD(test_08b)
 	{
 		std::wstring s(L"ponedjeljak");
-		// delete all vowels 
+		// TODO: delete all vowels 
 		Assert::AreEqual(L"pndjljk", s.c_str());
 	}
 	TEST_METHOD(test_09)
 	{
 		struct exam { std::wstring name; int points, grade; };
 		std::vector<exam> v{ {L"Pero", 55, 2}, {L"Iva", 93, 5}, {L"Marko", 89, 5} };
-		// sort vector by grade, then by points
+		// TODO: sort vector by grade, then by points
 		Assert::AreEqual(L"Iva", v[0].name.c_str());
 		Assert::AreEqual(L"Marko", v[1].name.c_str());
 		Assert::AreEqual(L"Pero", v[2].name.c_str());
@@ -163,7 +162,7 @@ public:
 
 		using namespace std::chrono;
 		auto t1 = steady_clock::now();
-		// put median value in the middle of vector. fast.
+		// TODO: put median value in the middle of vector. fast.
 		auto t2 = steady_clock::now();
 		auto dur = duration_cast<milliseconds>(t2 - t1);
 		Assert::AreEqual(1000., v[v.size() / 2]); // median value
@@ -173,23 +172,23 @@ public:
 	{
 		struct employee { std::wstring name; int salary; };
 		std::vector<employee> v{ {L"Iva", 2000}, {L"Pero", 1000}, {L"Marko", 10000} };
-		// put employee with median salary in the middle of vector
+		// TODO: put employee with median salary in the middle of vector
 		std::nth_element(v.begin(), v.begin() + v.size() / 2, v.end(), [](const employee& a, const employee& b) { return a.salary < b.salary; });
 		Assert::AreEqual(L"Iva", v[v.size() / 2].name.c_str()); // median_salary
 	}
 	TEST_METHOD(test_12)
 	{
 		std::vector<double> v{ 11, 0.5, -97.23, -23.11, 48.78, 22.96, -77 };
-		auto smallest_value = //
+		auto smallest_value = // TODO: 
 		Assert::AreEqual(-97.23, smallest_value);
-		auto largest_value = //
+		auto largest_value = // TODO: 
 		Assert::AreEqual(48.78, largest_value);
 	}
 	TEST_METHOD(test_13)
 	{
 		std::vector<int> atp_points { 8445, 7480, 6220, 5300, 5285 };
 		// the most interesting match is the one with the smallest difference
-		auto smallest_difference = //
+		auto smallest_difference = // TODO: 
 		Assert::AreEqual(15, smallest_difference);
 	}
 };
